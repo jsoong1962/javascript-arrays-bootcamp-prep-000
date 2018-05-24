fedbd945f9c9f5cbc75a8e5c0d1cc907b86a0d1c
Johnny Soong <johnnysoong@hotmail.com>
2018-05-24 21:00:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -58,7 +58,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1);
+  array.pop();
   return array;
 }
 
